d689ed46517c96d73035d4534d80b09dfc9c26c3
bintangnusantara388 <145913395+bintangnusantara388@users.noreply.github.com>
2023-10-26 12:41:49 +0700

    Add files via upload

---
+++ Latihan18.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan18;
+
+/**
+ *
+ * @author User
+ */
+public class Latihan18 {
+     public static void main(String[] args) {
+        double awal = 3.5;
+        double akhir = 35;
+        
+        for(double i=awal; i<= akhir; i+= 3.5){
+            System.out.println(i);
+      }
+}
+}
